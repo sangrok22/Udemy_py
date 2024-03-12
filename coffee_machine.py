48c46b0110d04f9da7f63e90f17b22b16c3329e5
SangRok Lee <shawn222@naver.com>
2024-03-12 23:43:40 +0900

    second commit

--- coffee_machine.py
+++ coffee_machine.py
@@ -25,24 +25,38 @@ MENU = {
         "cost": 3.0,
     }
 }
-choose = input("What do you like? (espresso/latte/cappuccino): ")
-print("Please insert coins.")
-quarters = int(input("how many quarters?: "))
-dimes = int(input("how many dimes?: "))
-nickles = int(input("how many nickles?: "))
-pennies = int(input("how many pennies?: "))
-
-pay= MENU[choose]["cost"]
-in_coin = (0.25*quarters)+(0.1*dimes)+(0.05*nickles)+(0.01*pennies)
-change = round(in_coin - pay ,2)
-if change < 0:
-    print("Sorry that's not enough money")
-else:
-    print(f"Here is ${change} in change.")
-
+profit = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
+
+
+should_continue = True
+
+while should_continue:
+    choose = input("What do you like? (espresso/latte/cappuccino): ")
+    if choose == "off":
+        break
+    elif choose == "report":
+        print(f"water: {resources['water']}m")
+        print(f"milk: {resources['milk']}ml")
+        print(f"coffee: {resources['coffee']}g")
+        print(f"money: ${profit}")
+
+    print("Please insert coins.")
+    quarters = int(input("how many quarters?: "))
+    dimes = int(input("how many dimes?: "))
+    nickles = int(input("how many nickles?: "))
+    pennies = int(input("how many pennies?: "))
+
+    pay= MENU[choose]["cost"]
+    in_coin = (0.25*quarters)+(0.1*dimes)+(0.05*nickles)+(0.01*pennies)
+    change = round(in_coin - pay ,2)
+    if change < 0:
+        print("Sorry that's not enough money")
+        should_continue = False
+    else:
+        print(f"Here is ${change} in change.")
